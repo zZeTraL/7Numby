bb00addf3b59dcb76d7c64e5a96d699585fbbed0
zZeTraL <lucashbs.pro@gmail.com>
2024-02-08 01:53:22 +0100

    🌠 Update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,12 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [react(), copy(
+    {
+      targets: [
+        {src: "src/data", dest: "dist"},
+      ],
+      hook: 'writeBundle'
+    }
+  )],
 })
